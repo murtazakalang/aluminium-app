08ba4f996dbb9c3479d0482df1c5acd9885ba09d
Murtaza Kalangwala <murtazakalangwala@Murtazas-MacBook-Pro.local>
2025-06-07 23:52:41 +0530

    Add docker-compose.yml and next.config.js to .gitignore

--- apps/frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone', // Enable standalone output
-  // Add other configurations here if needed
-};
-
-module.exports = nextConfig; 
